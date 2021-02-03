64c2b1b7e5a5bb7f811bce0f52762fa715362e25
RyuWoong <wjdfbd1121@gmail.com>
2021-02-03 23:09:44 +0900

    Update, ReviewModal

--- src/Components/Review/ReviewButton/ReviewButton.js
+++ src/Components/Review/ReviewButton/ReviewButton.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Button, Input, Modal, Radio } from 'antd';
+import { Button, Form, Input, Modal, Radio } from 'antd';
 import { FormOutlined } from '@ant-design/icons';
 import { useState } from 'react/cjs/react.development';
 
 const { TextArea } = Input;
 
-function ReviewButton() {
-  const [isVisible, setVisible] = useState(false);
-  const handleShowModal = () => {
-    setVisible(true);
-  };
+const CustomModal = ({ isVisible, setVisible }) => {
+  const [form] = Form.useForm();
+
   const handleOK = () => {
+    form.validateFields().then((values) => {
+      console.log(values);
+      form.resetFields();
+    });
     setVisible(false);
   };
 
@@ -18,19 +20,42 @@ function ReviewButton() {
     setVisible(false);
   };
   return (
+    <Modal
+      title="📝 평가남기기"
+      visible={isVisible}
+      okText="Submit"
+      onCancel={handleCancel}
+      onOk={handleOK}
+    >
+      <Form form={form} layout="vertical">
+        <Form.Item label="영화는 어땠나요?" name="Rate">
+          <Radio.Group size="large">
+            <Radio.Button value="1">👍 추천해요!</Radio.Button>
+            <Radio.Button value="0">🤔 평가보류</Radio.Button>
+            <Radio.Button value="-1">👎 별로예요.</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item label="생각을 간략하게 남겨주세요." name="Text">
+          <TextArea showCount maxLength={100} />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+function ReviewButton() {
+  const [isVisible, setVisible] = useState(false);
+  const handleShowModal = () => {
+    setVisible(true);
+  };
+
+  return (
     <>
       <Button onClick={handleShowModal}>
         <FormOutlined />
         평가하기
       </Button>
-      <Modal title="📝 평가남기기" visible={isVisible} onOk={handleOK} onCancel={handleCancel}>
-        <Radio.Group defaultValue="1" size="large">
-          <Radio.Button value="1">👍 추천해요!</Radio.Button>
-          <Radio.Button value="0">🤔 평가보류</Radio.Button>
-          <Radio.Button value="-1">👎 별로예요.</Radio.Button>
-        </Radio.Group>
-        <TextArea showCount maxLength={100} />
-      </Modal>
+      <CustomModal setVisible={setVisible} isVisible={isVisible} />
     </>
   );
 }
